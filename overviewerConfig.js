ac891d711734828ef8be28c39fbf73981f337bc1
Kenajcrap <fael-1999@hotmail.com>
2019-03-10 18:08:30 -0300

    base for mcmap

---
+++ overviewerConfig.js
@@ -0,0 +1,137 @@
+var overviewerConfig = {
+    "worlds": [
+        "Survival"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                0, 
+                17, 
+                0
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1552251033, 
+            "name": "Survival - Dia - 0", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Survival", 
+            "maxZoom": 6, 
+            "path": "upper-left", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                0, 
+                17, 
+                0
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1552251033, 
+            "name": "Survival - Dia - 90", 
+            "poititle": "Markers", 
+            "north_direction": 1, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Survival", 
+            "maxZoom": 6, 
+            "path": "upper-right", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                0, 
+                17, 
+                0
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1552251033, 
+            "name": "Survival - Dia - 180", 
+            "poititle": "Markers", 
+            "north_direction": 3, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Survival", 
+            "maxZoom": 6, 
+            "path": "lower-left", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                0, 
+                17, 
+                0
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1552251033, 
+            "name": "Survival - Dia - 270", 
+            "poititle": "Markers", 
+            "north_direction": 2, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "Survival", 
+            "maxZoom": 6, 
+            "path": "lower-right", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "queryMarker": "icons/marker_location.png", 
+            "spawnMarker": "icons/marker_home.png", 
+            "spawnMarker2x": "icons/marker_home_2x.png", 
+            "signMarker": "signpost_icon.png", 
+            "queryMarker2x": "icons/marker_location_2x.png", 
+            "compass": {
+                "0": "compass_upper-left.png", 
+                "1": "compass_upper-right.png", 
+                "2": "compass_lower-right.png", 
+                "3": "compass_lower-left.png"
+            }, 
+            "defaultMarker": "signpost.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "coordsBox": true, 
+            "zoom": true, 
+            "mapType": true, 
+            "compass": true, 
+            "overlays": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1552251748"
+    }
+};
