92a107415bb3a170293c47f9309b3d6404758698
Rafael Barros <rafaelbarrosr@outlook.com>
2019-03-14 21:49:23 -0300

    new_editor

---
+++ config.js
@@ -0,0 +1,103 @@
+var CONFIG = {
+  "maps": {
+    "Cidade_Principal": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1552610461,
+        0,
+        1552610461,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "Cidade_Principal",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "survival1_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "survival1",
+      "worldName": "survival1",
+      "worldSeaLevel": 64
+    },
+    "Mina_do_Kenaj": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1552610461,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 5,
+      "name": "Mina_do_Kenaj",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "survival2_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "survival2",
+      "worldName": "survival2",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "Cidade_Principal",
+    "Mina_do_Kenaj"
+  ],
+  "tileSetGroups": {
+    "survival1_isometric_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          -1,
+          3
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          31,
+          3
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "survival2_isometric_t1": {
+      "maxZoom": 5,
+      "tileOffsets": [
+        [
+          11,
+          -9
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
