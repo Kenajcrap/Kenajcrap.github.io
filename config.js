db091153d4508e421a8470633278db8021eb7e16
Rafael Barros <rafaelbarrosr@outlook.com>
2019-03-22 19:33:12 -0300

    Fri 03/22/2019 19:33:11.70

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var CONFIG = {
     "Cidade_Principal": {
       "imageFormat": "png",
       "lastRendered": [
-        1553289224,
+        1553293952,
         0,
-        1553289224,
+        1553293952,
         0
       ],
       "maxZoom": 6,
