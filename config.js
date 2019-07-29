d196952908d7c2e6ffdde93a5643fdfdacd55d8c
Rafael Barros <rafaelbarrosr@outlook.com>
2019-07-29 16:18:46 -0300

    Mon 07/29/2019 16:18:34.10

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ var CONFIG = {
       "defaultZoom": 5,
       "imageFormat": "png",
       "lastRendered": [
-        1564411008,
+        1564427843,
         0,
-        1564411008,
+        1564427843,
         0
       ],
       "maxZoom": 6,
