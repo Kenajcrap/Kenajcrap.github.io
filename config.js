4af3e562e084420f4b6f998fd71b3108e951769f
Rafael Barros <rafaelbarrosr@outlook.com>
2019-09-01 17:10:14 -0300

    Sun 09/01/2019 17:10:05.57

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ var CONFIG = {
       "defaultZoom": 5,
       "imageFormat": "png",
       "lastRendered": [
-        1566712028,
+        1567368459,
         0,
-        1566712028,
+        1567368459,
         0
       ],
       "maxZoom": 6,
@@ -34,7 +34,7 @@ var CONFIG = {
     "Farms - Gelo": {
       "imageFormat": "png",
       "lastRendered": [
-        1566712028,
+        1567368459,
         0,
         0,
         0
@@ -58,7 +58,7 @@ var CONFIG = {
     "Mina_do_Kenaj": {
       "imageFormat": "png",
       "lastRendered": [
-        1566712028,
+        1567368459,
         0,
         0,
         0
