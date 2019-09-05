fe26e26d419a65bf5134edca85de474c7c786c09
Rafael Barros <rafaelbarrosr@outlook.com>
2019-09-04 22:52:39 -0300

    Wed 09/04/2019 22:52:25.39

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ var CONFIG = {
       "defaultZoom": 8,
       "imageFormat": "png",
       "lastRendered": [
-        1567634405,
+        1567648211,
         0,
-        1567634405,
+        1567648211,
         0
       ],
       "maxZoom": 10,
@@ -34,7 +34,7 @@ var CONFIG = {
     "Mina_do_Kenaj": {
       "imageFormat": "png",
       "lastRendered": [
-        1567634405,
+        1567648211,
         0,
         0,
         0
